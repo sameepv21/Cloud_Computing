fb2b0e8665d2b37ee47497980f53636f76fdf6da
Sameep1234 <sameep0112vani@gmail.com>
2022-08-18 15:21:10 +0530

    chore: ip address for source code change

--- Extra_Programs/RMI/weather/WeatherServer.java
+++ Extra_Programs/RMI/weather/WeatherServer.java
@@ -9,51 +9,50 @@ import java.rmi.server.*;
 import java.net.*;
 
 public class WeatherServer extends UnicastRemoteObject
-    implements WeatherInterface {
+		implements WeatherInterface {
 
-    Weather w;
+	Weather w;
 
-    public WeatherServer() 
-	throws RemoteException {
-	// Weather(temp, wind, rain, stormy, sun, clouds, fog)
-	w = new Weather(18, 10, true, false, false, true, true);
-    }
-
-    // Implements the methods in WeatherInterface
-
-    public String type() 
-	throws RemoteException {
-	return w.getWeatherType();
-    }
-
-    public int temperature() 
-	throws RemoteException {
-	return w.getTemperature();
-    }
-
-    public int wind() 
-	throws RemoteException {
-	return w.getWind();
-    }
-
-    public Weather all() 
-	throws RemoteException {
-	return w;
-    }
-
-    // main-method to start the server.
-    public static void main(String args[]) {
-	try {
-	
-	    WeatherServer ws  = new WeatherServer();
-	    LocateRegistry.createRegistry(4711);
-	    Naming.rebind("//dslabsrv17.da-iict.ac.in:4711/WeatherServer", ws);
-	
-	} catch(RemoteException re) {
-	    System.out.println("Couldn not start up server.");
-	} catch(MalformedURLException mue) {
-	    System.out.println("The URL was malformed.");
+	public WeatherServer()
+			throws RemoteException {
+		// Weather(temp, wind, rain, stormy, sun, clouds, fog)
+		w = new Weather(18, 10, true, false, false, true, true);
 	}
-    }
-}
 
+	// Implements the methods in WeatherInterface
+
+	public String type()
+			throws RemoteException {
+		return w.getWeatherType();
+	}
+
+	public int temperature()
+			throws RemoteException {
+		return w.getTemperature();
+	}
+
+	public int wind()
+			throws RemoteException {
+		return w.getWind();
+	}
+
+	public Weather all()
+			throws RemoteException {
+		return w;
+	}
+
+	// main-method to start the server.
+	public static void main(String args[]) {
+		try {
+
+			WeatherServer ws = new WeatherServer();
+			LocateRegistry.createRegistry(1099);
+			Naming.rebind("//0.0.0.0:1099/WeatherServer", ws);
+
+		} catch (RemoteException re) {
+			System.out.println("Couldn not start up server.");
+		} catch (MalformedURLException mue) {
+			System.out.println("The URL was malformed.");
+		}
+	}
+}
